3e19c039220abe77945a6cfad024b6fcb8aabda1
rodz3rd2 <rodrigogalura3rd@gmail.com>
2019-02-11 08:48:20 +0800

    fix not watching js file

--- tasks/scripts-watch.js
+++ tasks/scripts-watch.js
@@ -2,8 +2,19 @@ var _ = require("underscore");
 
 module.exports = function (gulp, plugins, config, commands) {
     return function() {
-        gulp.watch(_.map(_.values(config.scripts.entries), function(entry) {
-            return "./" + entry;
-        }), [commands.scripts]);
+        var base = "";
+        if (typeof config.scripts.options !== "undefined") {
+            if (typeof config.scripts.options.base !== "undefined") {
+                base = config.scripts.options.base;
+            }
+        }
+
+        if (_.isEmpty(base)) {
+            gulp.watch(_.map(_.values(config.scripts.entries), function(entry) {
+                return "./" + (_.isEmpty(base) ? "" : base.replace("/\/$/", "") + "/") + entry;
+            }), [commands.scripts]);
+        } else {
+            gulp.watch(base.replace("/\/$/", "") + "/**/*.js", [commands.scripts]);
+        }
     };
 };
--- tasks/scripts.js
+++ tasks/scripts.js
@@ -5,8 +5,17 @@ var compiler = require("webpack"),
 
 module.exports = function (gulp, plugins, config) {
     return function () {
-        var options = (typeof config.scripts.options !== config.scripts.options) ? config.scripts.options : {};
-        var notify = typeof config.scripts.notify !== "undefined" ? config.scripts.notify : true;
+        var base = "";
+        var options = {};
+        if (typeof config.scripts.options !== "undefined") {
+            options = config.scripts.options;
+
+            if (typeof options.base !== "undefined") {
+                base = options.base;
+            }
+        }
+
+        var notify = (typeof config.scripts.notify !== "undefined") ? config.scripts.notify : true;
 
         return gulp.src("dummy-entry.js", _.extend({allowEmpty: true}, options))
                 .pipe(plugins.plumber({
@@ -19,7 +28,7 @@ module.exports = function (gulp, plugins, config) {
                     entry: _.object(
                         _.keys(config.scripts.entries),
                         _.map(_.values(config.scripts.entries), function(entry) {
-                            return "./" + entry;
+                            return "./" + (_.isEmpty(base) ? "" : base.replace("/\/$/", "") + "/") + entry;
                         })
                     ),
 
